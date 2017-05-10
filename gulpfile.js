daf17bb0c0736d60bcc48eb3b78465e3334902e5
msudol <msudol@anaren.com>
2017-05-10 16:08:30 -0400

    stub out gulp script to build a comprehensive demo.json

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+/*
+ * Atmosphere Demos Gulpfile
+ * 
+ * Description: Build atmosphere demos for endpoint apps with gulp on node.js 
+ * 
+ * gulp (runs the default gulp)
+ * 
+ * 
+ */ 
+
+
+// Vanilla Node Dependencies
+var del = require('del');
+var fs = require('fs');
+var merge = require('merge');
+var path = require('path');
+var gulp = require('gulp');
+
+
+// Gulp Specific Dependencies
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var zip = require('gulp-zip');
+
+
+// A general task object to make things cleaner
+var task = {}; 
+
+
+// Function to retrieve folders - something like this might work
+function getFolders(dir) {
+	return fs.readdirSync(dir)
+	.filter(function(file) {
+		return fs.statSync(path.join(dir, file)).isDirectory();
+	});
+}
+
+
+//TODO: create a gulp script to recurse the the */offical/*.atmo files and load up sufficient data for a demos.json file to serve the IDE
+gulp.task('build', function() {
+    
+    return null;
+    
+    
+});
+
+
+
+// The default tasks is scripts, so run scripts clean, which then runs scripts
+gulp.task('default', ['build']);
